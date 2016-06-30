74346bed0da09c5f242ec0645906a029448ae1c4
Thiago Moraes <tmoraes@tmoraes-mbp1.dhcp.thefacebook.com>
2016-06-30 19:40:07 -0300

    fixing https errors by removing fontawesome

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This material is aimed at teaching people how to implement Facebook's pixel on an e-commerce website. You can change this project however you want. If you need to start from scratch, just start over.
 
-All the design used in this training is from E-Shopper dowloaded from http://designscrazed.org/best-html-ecommerce-website-templates/
+All the design used in this training is from E-Shopper dowloaded from https//designscrazed.org/best-html-ecommerce-website-templates/
 
 # How to start working?
 
--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var app = express();
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
+// https//expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// https//expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
@@ -20,7 +20,7 @@ app.get("/dreams", function (request, response) {
   response.send(dreams);
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+// could also use the POST body instead of query string: https//expressjs.com/en/api.html#req.body
 app.post("/dreams", function (request, response) {
   dreams.push(request.query.dream);
   response.sendStatus(200);
